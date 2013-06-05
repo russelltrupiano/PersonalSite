7c08eb7f7a044fb7033e3dc3abaef205c9475cf6
russelltrupiano <russelltrupiano@gmail.com>
2013-06-05 14:27:31 -0400

    new layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+	var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+	var is_moz = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+	var is_safari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+
+	if (is_chrome || is_moz) {
+		$(".nav_item, .acc_title,.content p, .content_cat p").css({"font-weight": "bold"});
+	}
+	if (is_moz) {
+		$(".nav_item").css({"padding-left": "32px", "padding-right": "32px"});
+	}
+	else if (is_chrome) {
+		$(".nav_item").css({"padding-left": "35px", "padding-right": "35px"});
+	}
+	else if (is_safari) {
+		$(".nav_item").css({"padding-left": "39px", "padding-right": "39px"});
+	}
+
 	function resetAccordion() {
 		$("#content_education, #content_work, #content_interests").accordion({
 			collapsible: true,
@@ -14,9 +31,9 @@ $(document).ready(function() {
 	var newPage;
 	var alteredHeader = false;
 
-	$('#menu').effect("slide", 'slow');
+	// $('#menu').effect("slide", 'slow');
 	resetAccordion();
-	curPage.fadeTo(1200, 1);
+	curPage.fadeTo(1500, 1);
 
 	$(".colorBox").colorbox({
 		rel: 'viewer',
@@ -25,8 +42,10 @@ $(document).ready(function() {
 
 	$(".header_contents").mouseover(function() {
 		if (!alteredHeader) {
-			$(".hidden").css({"opacity":"1"});
-			$(".comments a, .hidden").animate({"color":"#ff8300", "opacity":".7"}, 1000);
+			// $(".hidden").show();
+			$(".hidden").animate({"opacity":"1"});
+			$(".comments a, .hidden").animate({"opacity":".7"}, 600);
+			// "color":"#ff8300", 
 			$(".no_comments").animate({"font-size":"+=20%"}, 400);
 			alteredHeader = true;
 		}
@@ -35,15 +54,15 @@ $(document).ready(function() {
 	$(".nav_item").click(function (event) {
 		newPage = $("#content"+event.target.id);
 		if (curPage.attr("id") != newPage.attr("id")) {
-			if (event.target.id != "_resume") {
+			// if (event.target.id != "_resume") {
 				curPage.hide();
 				curPage = newPage;
 				curPage.toggle('fade', 'easeInExpo', '1500');
 				resetAccordion();
 				$(".nav_item").removeClass("selectedNav");
 				$("#"+event.target.id).addClass("selectedNav");
-			}
-			else { open("docs/russelltrupiano.pdf");}
+			// }
+			// else { open("docs/russelltrupiano.pdf");}
 		}
 	});
 
